43ae777aae61c0419e8775e25b12a52501ca8b2f
DESKTOP-9EI3EKO\Kostas <test>
2021-02-13 14:28:41 +0000

    Create first step of purchasing a currency

--- ui/src/components/Purchase/Purchase.html.jsx
+++ ui/src/components/Purchase/Purchase.html.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Col, Form, InputGroup, Row } from 'react-bootstrap';
 import { Button, Step, Stepper, StepLabel, Typography, makeStyles } from '@material-ui/core';
+import Select from 'react-select'
+import { currencySymbols } from '../../utils/CurrencySymbols';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -22,7 +26,74 @@ function getStepContent(stepIndex, props) {
     switch (stepIndex) {
         case 0:
             return (
-                <></>
+                <Row className="justify-content-md-center">
+                    <Col lg="6" md="9" xs="12">
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label className="font-weight-bold">Buy</Form.Label>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Prepend style={{ width: props.data.selectedCurrency ? 200 : "100%" }}>
+                                        <Select
+                                            value={props.data.selectedCurrency}
+                                            options={props.data.currencies}
+                                            onChange={(e) => props.handleDropdownChange('selectedCurrency', e)}
+                                            getOptionLabel={(option) => {
+                                                return (
+                                                    <>
+                                                        <span className={`align-middle mr-2 currency-flag currency-flag-${option.name.toLowerCase()}`}></span>
+                                                        <span>{option.name}({currencySymbols[option.name]})</span>
+                                                    </>
+                                                )
+                                            }}
+                                            getOptionValue={(option) => option.name}
+                                            isClearable={true}
+                                            isSearchable={false}
+                                            styles={customStyles}
+                                            isLoading={props.data.isLoadingCurrencies}
+                                            placeholder="Select currency"
+                                        />
+                                    </InputGroup.Prepend>
+                                    {props.data.selectedCurrency &&
+                                        <Form.Control
+                                            type="number"
+                                            placeholder="Enter amount"
+                                            aria-label="purchaseAmount"
+                                            value={props.data.purchaseAmount}
+                                            onChange={(event) => { props.handleAmountChange(event, 'purchaseAmount') }}
+                                            isInvalid={props.data.insufficientBalance}
+                                            disabled={!props.data.selectedCurrency}
+                                            autoComplete="off"
+                                        />
+                                    }
+                                </InputGroup>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label className="font-weight-bold">Sell</Form.Label>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Prepend >
+                                        <InputGroup.Text style={{ width: 200 }}>
+                                            <span className={`align-middle mr-2 currency-flag currency-flag-gbp`}></span>
+                                            <span>GBP</span>
+                                        </InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Form.Control
+                                        type="number"
+                                        placeholder=""
+                                        aria-label="sellAmount"
+                                        aria-describedby="basic-addon1"
+                                        value={props.data.sellAmount}
+                                        disabled={true}
+                                        autoComplete="off"
+                                    />
+                                </InputGroup>
+                                <Form.Text className="text-muted">
+                                    Latest updated time: {props.data.time}
+                                </Form.Text>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                </Row>
             );
         case 1:
             return (
@@ -37,7 +108,20 @@ function getStepContent(stepIndex, props) {
     }
 }
 
-
+const customStyles = {
+    container: (provided) => ({
+        ...provided,
+        width: '100%'
+    }),
+    menu: (provided) => ({
+        ...provided,
+        backgroundColor: "whitesmoke"
+    }),
+    control: (provided) => ({
+        ...provided,
+        backgroundColor: "whitesmoke"
+    }),
+}
 
 function PurchaseHtml(props) {
 
@@ -83,7 +167,7 @@ function PurchaseHtml(props) {
                                 >
                                     Back
                                 </Button>
-                                <Button variant="contained" color="primary" onClick={handleNext}>
+                                <Button variant="contained" color="primary" onClick={handleNext} disabled={props.isButtonDisabled(activeStep)}>
                                     {activeStep === steps.length - 1 ? 'Confirm' : 'Next'}
                                 </Button>
                             </div>
--- ui/src/components/Purchase/Purchase.jsx
+++ ui/src/components/Purchase/Purchase.jsx
@@ -9,8 +9,13 @@ class PurchaseComponent extends Component {
         super();
         this.state = {
             time: '',
+            purchaseAmount: "",
+            sellAmount: "0.00",
             currencyRates: {},
             currencies: [],
+            selectedCurrency: null,
+            selectedRate: null,
+            isLoadingCurrencies: false,
         }
     }
 
@@ -32,11 +37,55 @@ class PurchaseComponent extends Component {
         })
     }
 
+    handleDropdownChange = (name, value) => {
+        const { currencyRates } = this.state;
+        const selectedRate = value && (1 / currencyRates[value.name]).toFixed(4);
+
+        this.setState({
+            [name]: value,
+            purchaseAmount: "",
+            selectedRate,
+            sellAmount: "0.00"
+        })
+    }
+
+    handleAmountChange = (e, text) => {
+        const input = e.target.value;
+
+        if (!input) {
+            this.setState({
+                [text]: input,
+                sellAmount: "0.00"
+            })
+        } else {
+            if (/^[1-9]\d*$/.test(input)) {
+                const { currencyRates, selectedRate } = this.state;
+                const convertedEURAmount = selectedRate * input;
+                const rateEURToGBP = currencyRates['GBP'];
+                const sellAmount = (rateEURToGBP * convertedEURAmount).toFixed(2);
+
+                this.setState({
+                    [text]: input,
+                    sellAmount: sellAmount
+                })
+            }
+        }
+    }
+
+    isButtonDisabled = (activeStep) => {
+        if ((activeStep === 0 && this.state.purchaseAmount)) {
+            return false;
+        }
+        return true;
+    }
 
     render() {
         return (
             <PurchaseHtml
                 data={this.state}
+                handleDropdownChange={this.handleDropdownChange}
+                handleAmountChange={this.handleAmountChange}
+                isButtonDisabled={this.isButtonDisabled}
             />
         )
     }
